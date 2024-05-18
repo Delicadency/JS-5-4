fe2d652b63da4006d2db31979f1b5a936a9b2a6a
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-18 19:04:14 +0200

    added alternative version of the same function

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function reverseArray(array) {
     return array2;
   }
 }
+
+function reverseArray2(array) {
+  if (!Array.isArray(array)) {
+    return "Nieprawidłowe wartości. Wprowadź dowolną tablicę.";
+  } else {
+    const array2 = array.toReversed();
+    return array2;
+  }
+}
